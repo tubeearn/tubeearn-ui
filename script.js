c61dfa89c8a963bd2279f9ef88d25959ce16bdf6
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-07-27 13:55:33 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMaintenance() {
+  document.getElementById('maintenancePopup').classList.remove('hidden');
+}
+
+function hideMaintenance() {
+  document.getElementById('maintenancePopup').classList.add('hidden');
+}
