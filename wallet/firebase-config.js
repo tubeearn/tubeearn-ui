506a78fdc6437de9616f710183cd24e56ce97d12
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-07-28 13:26:43 +0530

    Delete wallet directory

--- wallet/firebase-config.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
-  authDomain: "YOUR-PROJECT.firebaseapp.com",
-  projectId: "YOUR-PROJECT-ID",
-  storageBucket: "YOUR-PROJECT.appspot.com",
-  messagingSenderId: "YOUR-SENDER-ID",
-  appId: "YOUR-APP-ID"
-};
-firebase.initializeApp(firebaseConfig);
