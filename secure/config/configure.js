9f6295feb60f883bacfc07d828f7d4ab53421ada
tubeearn <wwwsanjeevbarh95@gmail.com>
2025-08-08 15:43:25 +0530

    configure.js

---
+++ secure/config/configure.js
@@ -0,0 +1,5 @@
+// secure/config/config.js
+window.appConfig = {
+  SUPABASE_URL: 'https://ejbvidirnsjvadvekede.supabase.co',
+  SUPABASE_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqYnZpZGlybnNqdmFkdmVrZWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MTA3ODMsImV4cCI6MjA2OTE4Njc4M30.KoCt8f2bXjn3843XOsRMLwyZ0lKpARkrRqb_GXlGfUs'
+};
